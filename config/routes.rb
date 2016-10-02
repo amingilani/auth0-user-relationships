a6e6d633931353f708ac43ffb9a8cb826dfc248e
amingilani <aminshahgilani@gmail.com>
2016-10-03 03:43:31 +0500

    Generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'auth0/callback'
+
+  get 'auth0/failure'
+
+  get 'dashboard/show'
+
+  get 'public_pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
